fad47e1b6b9b05c0c8af418b3f13049159e6afab
Raúl Hernández <raulhernandeziglesiass@gmail.com>
2021-01-10 17:29:26 +0100

    Movil: jugabilidad adaptada a maquina de estados

--- Assets/Scripts/Mobile Scripts/MobileCharacter.cs
+++ Assets/Scripts/Mobile Scripts/MobileCharacter.cs
@@ -21,20 +21,23 @@ public class MobileCharacter : MonoBehaviour
     [SerializeField] private Joystick moveJoystick;
     [SerializeField] private Joystick rotateJoystick;
     [SerializeField] private Button mode;
+    [SerializeField] private Sprite[] modeSprite;
     private Character characterScript;
     private bool shoot;
     private bool activeShield;
+    private int currentMode;
+    private bool stickState;
 
     private void Awake()
     {
         //if (SystemInfo.deviceType != DeviceType.Handheld)
-        if(!Application.isMobilePlatform)
+        /*if(!Application.isMobilePlatform)
         {
             moveJoystick.gameObject.SetActive(false);
             rotateJoystick.gameObject.SetActive(false);
             mode.gameObject.SetActive(false);
             GetComponent<MobileCharacter>().enabled = false;
-        }
+        }*/
     }
 
     // Start is called before the first frame update
@@ -43,31 +46,14 @@ public class MobileCharacter : MonoBehaviour
         rigidbody = character.GetComponent<Rigidbody>();
         characterScript = character.GetComponent<Character>();
         shoot = true;
+        currentMode = 1;
+        stickState = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if(Input.touchCount > 0)
-        {
-            foreach(Touch touch in Input.touches)
-            {
-                if (touch.position.x > middle.transform.position.x && !rotateJoystick.gameObject.activeInHierarchy) //Derecha
-                {
-                    rightTouchID = touch.fingerId;
-                    rotateJoystick.transform.position = new Vector3(touch.position.x, rotateJoystick.transform.position.y, touch.position.y);
-                    rotateJoystick.gameObject.SetActive(true);
-                }
-                else if(touch.position.x <= middle.transform.position.x && !moveJoystick.gameObject.activeInHierarchy)//Izquierda
-                {
-                    leftTouchID = touch.fingerId;
-                    moveJoystick.transform.position = new Vector3(touch.position.x, rotateJoystick.transform.position.y, touch.position.y);
-                    moveJoystick.gameObject.SetActive(true);
-                }
-            }
-        }*/
-        
-
+       
         verticalMove = moveJoystick.Vertical * speed;
         horizontalMove = moveJoystick.Horizontal * speed;
 
@@ -76,45 +62,57 @@ public class MobileCharacter : MonoBehaviour
 
         character.transform.LookAt(character.transform.position + joystickRotation);
         character.transform.position += new Vector3(horizontalMove, 0, verticalMove) * Time.deltaTime;
-        if(rotateJoystick.Horizontal != 0.0f || rotateJoystick.Vertical != 0.0f){
+        if(!stickState && rotateJoystick.Horizontal != 0.0f || rotateJoystick.Vertical != 0.0f){
             if (shoot)
             {
                 Debug.Log("Disparando joystick");
-                characterScript.Shoot();
+                characterScript.GetInputController().shootInput = true;
+                characterScript.movementSM.CurrentState.OnShoot();
             }
-            else if(!activeShield && !shoot)
+            else
             {
                 Debug.Log("Escudo joystick");
-                activeShield = true;
-                characterScript.CreateShield();
+                characterScript.GetInputController().shieldInput = true;
+                characterScript.movementSM.CurrentState.OnShield();
             }
+            stickState = true;
         }
-        else if(activeShield)
+        else if(stickState)
+        {
+            if (shoot)
+            {
+                characterScript.GetInputController().shootInput = false;
+                characterScript.movementSM.CurrentState.OnShoot();
+            }
+            else
+            {
+                characterScript.GetInputController().shieldInput = false;
+                characterScript.movementSM.CurrentState.OnShield();
+            }
+            stickState = false;
+        }
+        /*else if(activeShield)
         {
             characterScript.RemoveShield();
-        }
 
-
-
-
-        /*foreach(Touch t in Input.touches)
-        {
-            if(t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled)
-            {
-                if(t.fingerId == rightTouchID)
-                {
-                    rotateJoystick.gameObject.SetActive(false);
-                }
-                else if(t.fingerId == leftTouchID)
-                {
-                    moveJoystick.gameObject.SetActive(false);
-                }
-            }
         }*/
+
+
     }
 
     public void changeMode()
     {
+        switch (currentMode)
+        {
+            case 0:
+                currentMode = 1;
+                break;
+
+            case 1:
+                currentMode = 0;
+                break;
+        }
+        mode.GetComponent<Image>().sprite = modeSprite[currentMode];
         shoot = !shoot;
         Debug.Log("Modo: " + shoot);
     }
