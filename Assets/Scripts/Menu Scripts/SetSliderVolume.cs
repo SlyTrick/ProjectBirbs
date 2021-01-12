5e23e09bd9fb467545ab38f18ccf1c18af92c6bd
Raúl Hernández <raulhernandeziglesiass@gmail.com>
2021-01-12 13:04:32 +0100

    Menu de ajustes, slider funcional y cambios en fuentes de letra del menu

---
+++ Assets/Scripts/Menu Scripts/SetSliderVolume.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetSliderVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider slider;
+    public string valueName;
+
+    void Start()
+    {
+        float value = PlayerPrefs.GetFloat(valueName, 0.75f);
+        slider.value = value;
+        mixer.SetFloat(valueName, Mathf.Log10(value) * 20);
+        Debug.Log("Cargo el valor " + slider.value + " en " + valueName);
+    }
+
+    public void SetLevel()
+    {
+        mixer.SetFloat(valueName, Mathf.Log10(slider.value) * 20);
+        
+        PlayerPrefs.SetFloat(valueName, slider.value);
+        Debug.Log("Guardo el valor " + slider.value + " en " + valueName);
+    }
+}
---
+++ Assets/Scripts/Menu Scripts/SetVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public string valueName;
+
+    void Start()
+    {
+        float value = PlayerPrefs.GetFloat(valueName, 0.75f);
+        mixer.SetFloat(valueName, Mathf.Log10(value) * 20);
+        Debug.Log("Cargo el valor " + value + " en " + valueName);
+    }
+}
